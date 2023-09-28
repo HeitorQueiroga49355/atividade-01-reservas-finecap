2a8e7771a5db9be01921b7a292bb574c8c03cbd7
Heitor Queiroga Duarte <heitorqueiroga1@hotmail.com>
2023-09-28 14:58:33 -0300

    finalização da atividade

--- finecap/views.py
+++ finecap/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import ReservaForm
 from .models import Reserva
+from django.core.paginator import Paginator
 
 
 def reserva_criar(request):
@@ -23,7 +24,7 @@ def reserva_listar(request):
     filter_value = request.GET.get('valor')
     filter_quitado = request.GET.get('quitado')
     filter_data = request.GET.get('data')
-
+    page_number = request.GET.get("page")
 
     if filter_name:
             reservas = reservas.filter(nome_empresa__icontains=filter_name)
@@ -35,10 +36,21 @@ def reserva_listar(request):
         reservas = reservas.filter(quitado=str(filter_quitado))
 
     if filter_data:
-            reservas = reservas.filter(data__gte=searched_date)
+            reservas = reservas.filter(data__gte=filter_data)
+    
+    paginator = Paginator(reservas, 5)
+    page_obj = paginator.get_page(page_number)
+
     context ={
-        'reservas':reservas
+        'reservas':page_obj,
+        'has_next': page_obj.has_next,
+        'has_previous': page_obj.has_previous
     }
+    if (page_number == None or page_number == ''):
+        context['next_page'] = '2'
+    else:
+        context['next_page'] = str(int(page_number) + 1)
+        context['previous_page'] = str(int(page_number) - 1)
     return render(request, "finecap/reservas.html",context)
 
 
